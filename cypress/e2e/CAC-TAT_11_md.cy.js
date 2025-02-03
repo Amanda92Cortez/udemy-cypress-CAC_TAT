03b6601b8db7f03334a6918a4bcb867200be5d91
Amanda92Cortez <amanda_cortez_2012@hotmail.com>
2025-02-03 13:28:40 -0300

    Configure cypress cloud

---
+++ cypress/e2e/CAC-TAT_11_md.cy.js
@@ -0,0 +1,26 @@
+describe("Central de Atendimento ao Cliente TAT - Cypress Cloud", () => {
+  beforeEach(() => {
+    cy.visit("/src/index.html"); // Acessando local
+  });
+
+  it("verifica o título da aplicação", () => {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+
+
+  // https://github.com/cypress-io/github-action#end-to-end-testing
+  // 
+
+
+
+
+
+
+  // Padrão pra preencher os campos
+  // it("seleciona um produto (Blog) por seu índice", function () {
+    // cy.fillMandatoryFieldsAndSubmit();
+    // cy.contains("button", "Enviar").click();
+  // });
+
+});
